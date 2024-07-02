4530cef4d1d7ad4b0adb515fcc88eaccbebe7564
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-02 16:20:34 +0600

    chore: update config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,33 +3,34 @@ import type { Config } from "tailwindcss";
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [],
   theme: {
     extend: {
       fontFamily: {
         ubuntu: ["var(--font-ubuntu)"],
-        ibm_plex_mono: ["var(--font-ibm-plex-mono)"],
+        "ibm-plex-mono": ["var(--font-ibm-plex-mono)"],
         ...fontFamily,
       },
-    },
-    screens: {
-      tablet: "640px",
-      laptop: "1024px",
-      desktop: "1280px",
     },
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      bg_one: "#292F36",
-      bg_two: "#1A1E23",
-      brand_one: "#12F7D6",
-      brand_two: "#98FAEC",
+      "bg-one": "#292F36",
+      "bg-two": "#1A1E23",
+      "brand-one": "#12F7D6",
+      "brand-two": "#98FAEC",
       grey: "#43454D",
       white: "#FFFFFF",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
     },
   },
 };

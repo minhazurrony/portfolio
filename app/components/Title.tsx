60d0e606c04c2aaf36697a1110511e93d56a324e
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-04 20:28:04 +0600

    feat: add title component

---
+++ app/components/Title.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="relative mb-4 inline-block">
+        <h1 className="inline-block border-b pb-2 font-ubuntu text-4xl text-brand-one">
+          Skills
+        </h1>
+        <div className="absolute left-0 h-2 w-2 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-brand-one"></div>
+        <div className="absolute right-0 h-2 w-2 -translate-y-1/2 translate-x-1/2 transform rounded-full bg-brand-one"></div>
+      </div>
+      <h4 className="text-center font-ibm-plex-mono text-white">
+        I am striving to never stop learning and improving
+      </h4>
+    </div>
+  );
+}
+
+export default Title;
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,7 @@
+export { default as ContainerWrapper } from "./ContainerWrapper";
+export { default as ScrollBottom } from "./ScrollBottom";
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
-export { default as ContainerWrapper } from "./ContainerWrapper";
 export { default as About } from "./About";
-export { default as ScrollBottom } from "./ScrollBottom";
+export { default as Skills } from "./Skills";
+export { default as Title } from "./Title";

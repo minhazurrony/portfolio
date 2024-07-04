c4847b33681502f6eff8506c76ee618b66541c71
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-04 23:43:51 +0600

    fix: typography issue

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -4,7 +4,7 @@ function Title() {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="relative mb-4 inline-block">
-        <h1 className="inline-block border-b pb-2 font-ubuntu text-4xl text-brand-one">
+        <h1 className="inline-block border-b pb-2 font-ubuntu text-6xl text-brand-one">
           Skills
         </h1>
         <div className="absolute left-0 h-2 w-2 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-brand-one"></div>

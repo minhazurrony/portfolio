814af1570f6cd116ecb692975aad7f0219bf0b5b
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-04 20:29:26 +0600

    fix: typograpy issue

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 function About() {
   return (
     <ContainerWrapper className="bg-bg-two bg-[url('/assets/images/about_bg.svg')] bg-cover bg-center bg-no-repeat p-[128px]">
-      <div className="lg:block mb-[128px] hidden">
+      <div className="mb-[128px] hidden lg:block">
         <ScrollBottom />
       </div>
-      <div className="xl:flex-row xl:gap-32 flex flex-col justify-between gap-16">
-        <div className="xl:w-3/5 w-full">
-          <h2 className="font-ubuntu border-brand-one mb-16 inline-block rounded-br-[40px] rounded-tl-[40px] border-2 px-10 py-4 text-6xl capitalize text-white">
+      <div className="flex flex-col justify-between gap-16 xl:flex-row xl:gap-32">
+        <div className="w-full xl:w-3/5">
+          <h2 className="mb-16 inline-block rounded-br-[40px] rounded-tl-[40px] border-2 border-brand-one px-10 py-4 font-ubuntu text-6xl capitalize text-white">
             about me
           </h2>
 
-          <pre className="bg-bg-one/40 font-ibm-plex-mono whitespace-pre-wrap break-words rounded-2xl px-10 py-6 text-white backdrop-blur-sm">
-            <span className="text-brand-one mb-4 block">{`<p>`}</span>
-            <span className="text-brand-one text-xl capitalize">hello!</span>
+          <pre className="whitespace-pre-wrap break-words rounded-2xl bg-bg-one/40 px-10 py-6 font-ibm-plex-mono text-white backdrop-blur-sm">
+            <span className="mb-4 block text-brand-one">{`<p>`}</span>
+            <span className="text-xl capitalize text-brand-one">hello!</span>
             <br />
             <br />
             My name is Minhazur Rahman and I specialize in web developement that
@@ -39,12 +39,12 @@ function About() {
             <br />
             <br />I like to have my perspective and belief systems challenged so
             that I see the world through new eyes.
-            <span className="text-brand-one mt-4 block">{`</p>`}</span>
+            <span className="mt-4 block text-brand-one">{`</p>`}</span>
           </pre>
         </div>
 
         <Image
-          className="md:max-h-[628px] w-2/5 flex-auto rounded-2xl object-cover"
+          className="w-2/5 flex-auto rounded-2xl object-cover md:max-h-[628px]"
           src={"/assets/images/about.jpg"}
           alt="about-image"
           width={150}

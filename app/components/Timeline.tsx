38ee4dab7f82e40f662a57f013826130c39fecad
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-05 19:54:10 +0600

    refactor: modify timeline component

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -18,7 +18,7 @@ const TimelineDetails = ({
   achievements,
 }: TimelineItem) => {
   return (
-    <>
+    <div className="flex-1 pb-[128px]">
       <h1 className="font-ubuntu text-2xl text-brand-one">{designation}</h1>
       <h4 className="font-ibm-plex-mono text-sm italic text-white">
         {companyName}
@@ -31,7 +31,7 @@ const TimelineDetails = ({
           return <li key={achievement}>{achievement}</li>;
         })}
       </ul>
-    </>
+    </div>
   );
 };
 
@@ -43,21 +43,26 @@ function Timeline({ data }: TimelineProps) {
 
         return (
           <div className="flex justify-between gap-8">
-            <div className="flex-1 pb-[128px]">
-              {!isEvenIndex ? (
-                <div className="flex flex-col items-end">
-                  <div>
-                    <TimelineDetails {...item} />
-                  </div>
+            {!isEvenIndex ? (
+              <div className="flex flex-1 flex-col items-end">
+                <div>
+                  <TimelineDetails {...item} />
                 </div>
-              ) : null}
-            </div>
+              </div>
+            ) : (
+              <div className="flex-1"></div>
+            )}
+
             <div className="relative w-1 bg-brand-one">
               <div className="absolute left-0 top-0 h-4 w-4 -translate-x-[41%] rounded-full bg-brand-one"></div>
             </div>
-            <div className="flex-1 pb-[128px]">
-              {isEvenIndex ? <TimelineDetails {...item} /> : null}
-            </div>
+            <>
+              {isEvenIndex ? (
+                <TimelineDetails {...item} />
+              ) : (
+                <div className="flex-1"></div>
+              )}
+            </>
           </div>
         );
       })}

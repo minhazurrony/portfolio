203d97eb9224a9d280f06e39b21facc4204d10b3
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-05 14:26:11 +0600

    feat: add some svg icons

---
+++ public/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as NextIcon } from "./Nextjs";
+export { default as AntdIcon } from "./Antd";
+export { default as TsIcon } from "./TypeScript";
+export { default as TailwindIcon } from "./Tailwind";
+export { default as HtmlIcon } from "./Html";
+export { default as CssIcon } from "./Css";

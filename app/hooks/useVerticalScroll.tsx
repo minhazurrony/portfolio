bb594227a843ba41f0808ec422be501ef6851383
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-08 15:51:29 +0600

    feat: add a hook to detect vertical scrolling or not

---
+++ app/hooks/useVerticalScroll.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+export function useVerticalScroll() {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return {
+    scrolling,
+  };
+}

2160c47ccb397a9ce76676a78a38ac519d69f1a0
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-16 17:55:35 +0600

    feat: add infinity scroll animation into skills section

--- app/containers/home-page/Skills.tsx
+++ app/containers/home-page/Skills.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import { ContainerWrapper, ScrollBottom, Skill, Title } from "@/components";
 import { SKILLS } from "@/constants/skills";
+import { animate, m, useMotionValue } from "framer-motion";
 
 function Skills() {
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const xTranslation = useMotionValue(0);
+
+  useEffect(() => {
+    const width = sliderRef.current!.offsetWidth;
+    const finalPosition = -width / 2 - 24;
+    const controls = animate(xTranslation, finalPosition, {
+      ease: "linear",
+      duration: 25,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+
+    return () => {
+      controls.stop();
+    };
+  }, []);
+
   return (
     <ContainerWrapper className="bg-bg-one p-[128px]">
       <div className="mb-[128px] hidden lg:block">
@@ -15,15 +36,21 @@ function Skills() {
         />
       </div>
 
-      <div className="grid grid-cols-3 gap-16 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {SKILLS.map((skill) => (
-          <Skill
-            key={skill.name}
-            label={skill.name}
-            icon={skill.icon}
-            iconColor={skill.iconColor}
-          />
-        ))}
+      <div className="gradient-mask relative mx-auto w-full overflow-hidden py-4 lg:w-4/5">
+        <m.div
+          className="flex w-fit gap-12"
+          style={{ x: xTranslation }}
+          ref={sliderRef}
+        >
+          {[...SKILLS, ...SKILLS].map((skill) => (
+            <Skill
+              key={skill.name}
+              label={skill.name}
+              icon={skill.icon}
+              iconColor={skill.iconColor}
+            />
+          ))}
+        </m.div>
       </div>
     </ContainerWrapper>
   );
